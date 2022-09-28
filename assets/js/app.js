617a73e3fd9d03aa46e430408179678f63974d03
charo7507 <charolite.technologie@gmail.com>
2022-09-29 01:42:35 +0200

    finition de la page home

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const header = document.getElementById('header')
+
+window.addEventListener('scroll', () =>{
+    if (window.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    } else {
+        header.classList.remove('scroll-header')
+    }
+})
+
