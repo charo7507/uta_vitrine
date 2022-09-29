23190278b01d1cdcad8e55f31eba1540f01469b5
charo7507 <charolite.technologie@gmail.com>
2022-09-29 02:56:09 +0200

    finition de la page poplar

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,29 @@ window.addEventListener('scroll', () =>{
     }
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".popular__container", {
+    spaceBetween: 32, 
+    // grabCursor: true,
+    // centeredSlides: true,
+    // slidesPerView: 'auto',
+    // loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
